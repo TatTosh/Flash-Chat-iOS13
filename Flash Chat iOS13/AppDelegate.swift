20474bca4b84e536ce3820edb4a95df7209cbcc4
Tatjana Urvan <tatjana.urvan@nl.abnamro.com>
2023-12-22 11:43:18 +0100

    Improve message loading, add keyboard shuffling

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -6,8 +6,10 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
+import Firebase
+import IQKeyboardManagerSwift
 import UIKit
-import FirebaseCore
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		
 		FirebaseApp.configure()
+		let db = Firestore.firestore()
+		print(db)
+		
+		IQKeyboardManager.shared.enable = true
+		IQKeyboardManager.shared.enableAutoToolbar = false
+		IQKeyboardManager.shared.shouldResignOnTouchOutside = true
 		
         return true
     }
--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,9 @@ class ChatViewController: UIViewController {
 	@IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var messageTextfield: UITextField!
 	
-	var messages: [Message] = [
-		Message(sender: "test@gmail.com", body: "Hey"),
-		Message(sender: "test1@gmail.com", body: "Hello"),
-		Message(sender: "test@gmail.com", body: "What's up? Long text to test resizing of the cell depending on amount of text")
-	]
+	let db = Firestore.firestore()
+	
+	var messages: [Message] = []
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -28,9 +26,52 @@ class ChatViewController: UIViewController {
 		navigationItem.hidesBackButton = true
 		
 		tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+		
+		loadMessages()
+	}
+	
+	func loadMessages() {
+		
+		db.collection(K.FStore.collectionName)
+			.order(by: K.FStore.dateField)
+			.addSnapshotListener { (querySnapshot, error) in
+			self.messages = []
+
+			if let e = error {
+				print("There was an issue retreiving data from Firestore \(e)")
+			} else {
+				if let snapshotDocuments = querySnapshot?.documents {
+					for doc in snapshotDocuments {
+						let data = doc.data()
+						if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+							
+							let newMessage = Message(sender: messageSender, body: messageBody)
+							self.messages.append(newMessage)
+							
+							DispatchQueue.main.async {
+								self.tableView.reloadData()
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	@IBAction func sendPressed(_ sender: UIButton) {
+		if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+			db.collection(K.FStore.collectionName).addDocument(data: [
+				K.FStore.senderField: messageSender,
+				K.FStore.bodyField: messageBody,
+				K.FStore.dateField: Date().timeIntervalSince1970
+			]) { (error) in
+				if let e = error {
+					print("There was an issue saving data to Firestore, \(e)")
+				} else {
+					print("Successfully saved data")
+				}
+			}
+		}
 	}
 	
 	@IBAction func logOutPressed(_ sender: UIBarButtonItem) {
